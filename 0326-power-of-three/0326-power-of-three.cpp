0094d11e7f881d44db03d7a916f150509f39cca4
Satvik <satvikshrivastava2005@gmail.com>
2026-08-31 18:40:36 +0530

    Time: 5 ms (31.37%), Space: 8.8 MB (75.35%) - LeetHub - Time: O(log n), Space: O(1)

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -10,3 +10,7 @@ public:
         return n == 1;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
