f6fc4380507efa2b617e475b8f957c663acf1677
Satvik <satvikshrivastava2005@gmail.com>
2026-09-02 16:12:24 +0530

    Time: 0 ms (100%), Space: 8.1 MB (99.28%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        bitset<32>b(n);
+        return b.count();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
