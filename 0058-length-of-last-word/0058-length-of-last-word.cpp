39af782826721a835ad8c948f4984f0ae8df9eb6
Satvik <satvikshrivastava2005@gmail.com>
2026-09-05 16:39:27 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.49%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int count = 0;
-        for (int i = s.size() - 1; i >=0; i--) {
-            if (s[i] == ' ') {
-                if (count > 0)
-                    return count;
-            }
-            else {
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
                 count++;
-            }
-        }
-
+            } else if (count > 0) {
+                break;
+            }}
         return count;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
