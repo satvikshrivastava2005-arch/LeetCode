e630a87e5a21a2ac9586d259eac8735ad20f38cd
Satvik <satvikshrivastava2005@gmail.com>
2026-09-08 22:12:12 +0530

    Time: 0 ms (100%), Space: 10.3 MB (36.54%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0 ; i< n; i++){
+            for(int j = i+1 ; j < n ; j++){
+                swap(matrix[i][j] , matrix[j][i] );
+            }
+        }
+        for (int i = 0;i < n; i++) {
+            reverse(matrix[i].begin() ,matrix[i].end());}
+    }
+}; 
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
