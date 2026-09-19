3fa4411ea9889f14f6c4ac52bd0ac78ab9336867
Satvik <satvikshrivastava2005@gmail.com>
2026-09-19 13:32:10 +0530

    Time: 7 ms (11.48%), Space: 42.1 MB (5.73%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+      sort(nums.begin(), nums.end());
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        
+        int freq = 1;
+        int ans = nums[0];
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                freq++;
+            } else {
+                freq = 1; 
+                ans = nums[i];
+            } 
+            
+            if (freq > n / 2) {
+                return ans;    
+            } 
+        }
+        
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
